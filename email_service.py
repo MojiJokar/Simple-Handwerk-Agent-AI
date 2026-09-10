f92acbe19f87f9194ffcbb4befe055c8a6c2c768
MaxJokar <mmb2020max@gmail.com>
2026-09-10 11:13:22 +0200

    email_service.py created

---
+++ email_service.py
@@ -0,0 +1,15 @@
+def get_new_email():
+
+    return """
+    Hallo,
+
+    meine Heizung funktioniert nicht.
+
+    Ich wohne in Heidelberg.
+
+    Können Sie morgen kommen?
+
+    Viele Grüße
+
+    Max Müller
+    """
