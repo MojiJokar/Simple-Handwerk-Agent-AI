685e1a0c3de7319bd6975e9703428d17c0669407
MaxJokar <mmb2020max@gmail.com>
2026-09-10 11:12:18 +0200

    prompts.py created

---
+++ crm.py
@@ -0,0 +1,47 @@
+import json
+import os
+
+
+class CRM:
+
+    def __init__(self, filename="data/crm.json"):
+
+        self.filename = filename
+
+        if not os.path.exists(self.filename):
+
+            with open(self.filename, "w") as file:
+                json.dump([], file)
+
+    def get_customers(self):
+
+        with open(self.filename, "r") as file:
+            return json.load(file)
+
+    def find_customer(self, email):
+
+        customers = self.get_customers()
+
+        for customer in customers:
+
+            if customer["email"] == email:
+                return customer
+
+        return None
+
+    def create_customer(self, customer):
+
+        customers = self.get_customers()
+
+        customers.append(customer)
+
+        with open(self.filename, "w") as file:
+
+            json.dump(
+                customers,
+                file,
+                indent=4,
+                ensure_ascii=False
+            )
+
+        return customer
